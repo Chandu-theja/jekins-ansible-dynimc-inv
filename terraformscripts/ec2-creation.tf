ad8b555982e9fc563e4ab0f07b367a6d84065996
Chandu-theja <110975995+Chandu-theja@users.noreply.github.com>
2023-12-05 17:09:39 +0530

    Update ec2-creation.tf

--- terraformscripts/ec2-creation.tf
+++ terraformscripts/ec2-creation.tf
@@ -6,12 +6,12 @@ region = "ap-south-1"
 resource "aws_instance" "AWSInstance" {
      count ="2"
      ami = "ami-080e56c34f8367264"
-	 instance_type = "t2.micro"
-	 key_name = "devopskey"
-	 security_groups = ["launch-wizard-1"]
-	 tags = {
-	 Name = "tomcatservers"
-	 }
+     instance_type = "t2.micro"
+     key_name = "devopskey"
+     security_groups = ["launch-wizard-1"]
+     tags = {
+      Name = "tomcatservers"
+}
 }
 
 
