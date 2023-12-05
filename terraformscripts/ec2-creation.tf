a32fe0a1e6ee90c4cc429a3d09128d4bd2baf5cd
Chandu-theja <110975995+Chandu-theja@users.noreply.github.com>
2023-12-05 15:09:50 +0530

    Update ec2-creation.tf

--- terraformscripts/ec2-creation.tf
+++ terraformscripts/ec2-creation.tf
@@ -3,6 +3,9 @@ provider "aws"{
 region = "ap-south-1"
 }
 
+export AWS_ACCESS_KEY_ID=${aws-credentials}
+export AWS_SECRET_ACCESS_KEY=${aws-credentials}
+
 resource "aws_instance" "AWSInstance" {
      count ="2"
      ami = "ami-04708942c263d8190"
